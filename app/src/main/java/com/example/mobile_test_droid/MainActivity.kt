295ab4d2ab9e6485a6381bd886191066f0508c55
kogevnikovserg@gmail.com <kogevnikovserg@gmail.com>
2021-05-14 16:06:58 +0500

    async load files

--- app/src/main/java/com/example/mobile_test_droid/MainActivity.kt
+++ app/src/main/java/com/example/mobile_test_droid/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.mobile_test_droid
 //import android.R
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
@@ -11,9 +13,12 @@ import android.widget.*
 //import android.view.LayoutInflater
 //import android.widget.ArrayAdapter
 import android.view.View
+import androidx.annotation.RequiresApi
 //import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.doOnTextChanged
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.google.android.material.snackbar.Snackbar
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import java.io.*
@@ -21,48 +26,7 @@ import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.util.*
 
-
-class EdPeriod() {
-    public lateinit var start: String
-    public lateinit var end: String
-}
-
-class Person() {
-    public lateinit var id: String
-    public lateinit var name: String
-    public lateinit var phone: String
-    public lateinit var height: Number
-    public lateinit var biography: String
-    public lateinit var temperament: String
-    public lateinit var educationPeriod: EdPeriod
-}
-
-//class PersonAdapter(context: Context, resource: Int, list: ArrayList<Person>):
-//    ArrayAdapter<Person>(context, resource, list) {
-//
-//    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-//        val inflater = LayoutInflater.from(context)
-//        val rowView = inflater.inflate(R.layout.custom_list, null, true)
-//        val titleText = rowView.findViewById(R.id.textView1) as TextView
-//        val subtitleText = rowView.findViewById(R.id.textView2) as TextView
-//        val persona = getItem(position)
-//        titleText.setText(persona?.name)
-//        subtitleText.setText(persona?.phone)
-//
-//        return rowView
-//
-////        val persona = getItem(position)
-//////        if (convertView === null) {
-//////            convertView = LayoutInflater.from(getContext()).inflate(R.layout.simple_list_item_2, parent, false)
-//////        }
-////
-////        val tvName = convertView.findViewById(R.id.textView1)
-////        val tvPhone = convertView.findViewById(R.id.textView2)
-////        tvName.setText(persona?.name)
-////        tvPhone.setText(persona?.phone)
-////        return convertView
-//    }
-//}
+@RequiresApi(Build.VERSION_CODES.O)
 fun parseDate(date: String): String {
     val newDate = LocalDate.parse(date.substring(0, 10), DateTimeFormatter.ofPattern("yyyy-MM-dd"))
     newDate.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))
@@ -74,6 +38,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var mListView: ListView
     private var count: Int = 0
     private lateinit var dataPersons: MutableList<Person>
+    private lateinit var handler: Handler
     private fun readRawTextFile(context: Context, resId: Int): String? {
         val inputStream = context.resources.openRawResource(resId)
         val inputReader = InputStreamReader(inputStream)
@@ -95,6 +60,7 @@ class MainActivity : AppCompatActivity() {
         var fileId: Int = context.getResources().getIdentifier("generated01", "raw", getPackageName())
         var data = readRawTextFile(context, fileId)
         val listPersonType = object : TypeToken<MutableList<Person>>() {}.type
+
         dataPersons = gson.fromJson(data, listPersonType)
         fileId = context.getResources().getIdentifier("generated02", "raw", getPackageName())
         data = readRawTextFile(context, fileId)
@@ -103,47 +69,59 @@ class MainActivity : AppCompatActivity() {
         data = readRawTextFile(context, fileId)
         dataPersons.union(gson.fromJson(data, listPersonType))
     }
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        handler = Handler()
+        val handlerLoadData = Handler()
         mSearchInput = findViewById(R.id.search_input)
         val context: Context = getBaseContext()
-        loadData(context)
-        val pb: ProgressBar = findViewById(R.id.progressBar)
-        pb.setVisibility(View.GONE)
-        Log.i("mactivity dataPersons", "${dataPersons.size}")
+        handlerLoadData.postDelayed(Runnable{
+            loadData(context)
+            val pb: ProgressBar = findViewById(R.id.progressBar)
+            pb.setVisibility(View.GONE)
+            Log.i("mactivity dataPersons", "${dataPersons.size}")
 //        dataPersons.forEachIndexed { idx, person -> Log.i("data", "> Item $idx:\n$person") }
-        mListView = findViewById(R.id.person_list)
-        val adapter = MyListAdapter(this, R.layout.custom_list, dataPersons)
-        mListView.setAdapter(adapter)
-        mListView.setOnItemClickListener {
-            parent: AdapterView<*>?, view: View?,
-                position: Int, id: Long ->
-            val person = dataPersons[position]
-            val intent = Intent(this, PersonActivity::class.java)
+            mListView = findViewById(R.id.person_list)
+            val adapter = MyListAdapter(this, R.layout.custom_list, dataPersons)
+            mListView.setAdapter(adapter)
+            mListView.setOnItemClickListener {
+                    parent: AdapterView<*>?, view: View?,
+                    position: Int, id: Long ->
+                val person = dataPersons[position]
+                val intent = Intent(this, PersonActivity::class.java)
 //            Log.i("mactivity p name", person.name)
-            intent.putExtra(PersonActivity.NAME, person.name)
-            intent.putExtra(PersonActivity.PHONE, person.phone)
-            intent.putExtra(PersonActivity.BIO, person.biography)
-            val ePeriod = person.educationPeriod
-            intent.putExtra(PersonActivity.PERIOD, "${parseDate(ePeriod.start)} - ${parseDate(ePeriod.end)}")
-            intent.putExtra(PersonActivity.TEMPERAMENT, person.temperament)
-            startActivity(intent)
+                intent.putExtra(PersonActivity.NAME, person.name)
+                intent.putExtra(PersonActivity.PHONE, person.phone)
+                intent.putExtra(PersonActivity.BIO, person.biography)
+                val ePeriod = person.educationPeriod
+                intent.putExtra(PersonActivity.PERIOD, "${parseDate(ePeriod.start)} - ${parseDate(ePeriod.end)}")
+                intent.putExtra(PersonActivity.TEMPERAMENT, person.temperament)
+                startActivity(intent)
 //            Toast.makeText(this, "Position: ${position}: ${dataPersons[position]}", Toast.LENGTH_LONG).show()
-        }
-        mSearchInput.addTextChangedListener(object: TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
             }
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                Log.i("mactivity ontextchanged", "${s}")
-                adapter.getFilter().filter(s)
-            }
-            override fun afterTextChanged(s: Editable?) {
-            }
-        })
+            mSearchInput.addTextChangedListener(object: TextWatcher {
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                }
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    Log.i("mactivity ontextchanged", "${s}")
+                    adapter.getFilter().filter(s)
+                }
+                override fun afterTextChanged(s: Editable?) {
+                }
+            })
+        }, 1000L)
         val mCloseButton: ImageButton = findViewById(R.id.drop_search_button)
         mCloseButton.setOnClickListener {
             mSearchInput.setText("")
+        }
+        val mSwipeRefreshLayout: SwipeRefreshLayout = findViewById(R.id.swiperefresh)
+        mSwipeRefreshLayout.setOnRefreshListener {
+            handler.postDelayed(Runnable {
+                Snackbar.make(findViewById(R.id.content_main_layout), "Нет подключения сети", Snackbar.LENGTH_LONG).show()
+                mSwipeRefreshLayout.isRefreshing = false
+            }, 3000.toLong())
         }
     }
 }
---
+++ app/src/main/java/com/example/mobile_test_droid/Person.kt
@@ -0,0 +1,15 @@
+package com.example.mobile_test_droid
+
+class EdPeriod() {
+    public lateinit var start: String
+    public lateinit var end: String
+}
+class Person {
+    public lateinit var id: String
+    public lateinit var name: String
+    public lateinit var phone: String
+    public lateinit var height: Number
+    public lateinit var biography: String
+    public lateinit var temperament: String
+    public lateinit var educationPeriod: EdPeriod
+}
